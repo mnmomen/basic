983593f396ce94cdf27a1674bfa950cdb305a3a2
Momen <Momen@192.168.2.46>
2019-10-30 13:55:38 -0400

    any

---
+++ BasicJava/BasicJava/BasicJava/BioData.java
@@ -0,0 +1,7 @@
+package BasicJava;
+
+public class BioData {
+	public static void main(String[] args) {
+		System.out.print("momen");
+	}
+}
---
+++ BasicJava/BasicJava/BioData.java
@@ -0,0 +1,12 @@
+package BasicJava;
+
+public class BioData {
+	
+	public static void main(String[] args) {}
+	//public static void main (String [] args) {
+		
+	System.out.print("momen");
+		
+	}
+
+}
